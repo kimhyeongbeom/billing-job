01eaa2ce77073099ffc9d3bb34073f5c4f872e4b
hyeongbeomkim <hyeongbeomkim@gmail.com>
2024-06-30 15:57:16 +0900

    #2-4 Job Instances의 이해 (실습)

--- billing-job/src/main/java/example/billingjob/BillingJob.java
+++ billing-job/src/main/java/example/billingjob/BillingJob.java
@@ -1,9 +1,6 @@
 package example.billingjob;
 
-import org.springframework.batch.core.BatchStatus;
-import org.springframework.batch.core.ExitStatus;
-import org.springframework.batch.core.Job;
-import org.springframework.batch.core.JobExecution;
+import org.springframework.batch.core.*;
 import org.springframework.batch.core.repository.JobRepository;
 
 public class BillingJob implements Job {
@@ -21,14 +18,11 @@ public class BillingJob implements Job {
 
     @Override
     public void execute(JobExecution execution) {
-        try {
-            throw new Exception("Unable to process billing information");
-        } catch (Exception exception) {
-            execution.addFailureException(exception);
-            execution.setStatus(BatchStatus.COMPLETED);
-            execution.setExitStatus(ExitStatus.FAILED.addExitDescription(exception.getMessage()));
-        } finally {
-            this.jobRepository.update(execution);
-        }
+        JobParameters jobParameters = execution.getJobParameters();
+        String inputFile = jobParameters.getString("input.file");
+        System.out.println("processing billing information from file " + inputFile);
+        execution.setStatus(BatchStatus.COMPLETED);
+        execution.setExitStatus(ExitStatus.COMPLETED);
+        this.jobRepository.update(execution);
     }
 }
